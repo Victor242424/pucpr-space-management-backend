8d186db3395c1ed9a1bd8c1c5d0f64330afae257
Victor Rivas <vrivas@parameta.co>
2025-11-21 22:45:22 -0300

    Modificado cross origins permitidos

--- src/main/java/dev/victor_rivas/space_management/security/SecurityConfig.java
+++ src/main/java/dev/victor_rivas/space_management/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
