b5ec5a3311c1698c87dbbe0dcb7ef56542635cd2
Victor Rivas <vrivas@parameta.co>
2025-11-23 11:38:55 -0300

    Agregada obtencion de datos de .env

--- src/main/java/dev/victor_rivas/space_management/SpaceManagementApplication.java
+++ src/main/java/dev/victor_rivas/space_management/SpaceManagementApplication.java
@@ -1,5 +1,6 @@
 package dev.victor_rivas.space_management;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,7 +10,15 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class SpaceManagementApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(SpaceManagementApplication.class, args);
     }
-
 }
